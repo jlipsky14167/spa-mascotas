3130af82559b8ba55959db5351069d3d95a06471
Jefferson Peter Lipsky Godoy <jlipsky@brangerbriz.com>
2025-04-27 21:52:36 -0500

    Index mejorado Imagen arreglada

--- src/components/CompraConcentrados.jsx
+++ src/components/CompraConcentrados.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import dogImg from '../assets/images/dog.jpg'
 
 const CompraConcentrados = () => (
   <section id="alimentacion" className="bg-white p-4 rounded shadow-sm">
@@ -19,7 +20,7 @@ const CompraConcentrados = () => (
       <button type="submit" className="btn btn-warning">Registrar Compra</button>
     </form>
     <figure>
-      <img src="/spa-mascotas/src/assets/images/dog.jpg" alt="Perro tratando de entender" className="img-fluid my-3 rounded" />
+      <img src={dogImg} alt="Perro tratando de entender" className="img-fluid my-3 rounded" />
       <figcaption>
             <a
               href="https://www.freepik.com/free-photo/cute-dog-with-bright-brown-eyes-staring_10758899.htm#fromView=search&page=1&position=9&uuid=092c3698-8f14-4a08-abad-279ed45997e5&query=confused+dog"
